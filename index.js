6b19e981faca1f814f8904cf40decb1dfc285626
Edward <ed.f.jones@gmail.com>
2023-05-26 10:31:08 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //word = word.replace(/\w/g, "");
+  //word = word.toLowercase();
+  return word === word.split("").reverse().join("");
+
 }
 
 /* 
   Add your pseudocode here
+  return the words if the words are the same both backwards and forwards
 */
 
 /*
   Add written explanation of your solution here
-*/
 
+  We needed to see if these words were the same in reverse. So by reversing each string and seeing if they were
+  the same forward, we needed to use .reverse to check. .split was used just in case it was a phase and join was used
+  to make sure it was b roght together so that we have all bases covered
+*/
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
